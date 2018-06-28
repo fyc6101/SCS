c8d4002d1ed61a9b4482f50938d3e752c1591f93
fyc6101 <fych@hotmail.com>
2018-06-28 14:33:29 +0800

    提交文件

---
+++ Application/Teacher/Conf/config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+  'DB_TYPE'=>'mysql',
+  'DB_HOST'=>'127.0.0.1',
+  'DB_NAME'=>'wechat',
+  'DB_USER'=>'root',
+  'DB_PWD'=>'123456',
+  'DB_PORT'=>'3306',
+  'DB_PREFIX'=>'',
+  'DB_CHARSET'=>'utf8',
+
+  '__ROOT__'=>'Users/karl/Desktop/wechat_course',
+  'Time_Now'=>'2013-2014 秋季',
+);
